abdadb1168b8914ce4c709e3dc3245e292efd4de
Willian Barata <willianbarata@gmail.com>
2023-12-17 09:31:08 -0300

    Data Annotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace BlazorShop.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Informe o título")]
+        [MinLength(3, ErrorMessage = "A categoria deve ter pelo menos 3 caracteres")]
+        [MaxLength(60, ErrorMessage = "A categoria deve ter pelo menos 3 caracteres")]
         public string Title { get; set; } = string.Empty;
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,15 @@ namespace BlazorShop.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Informe o título")]
+        [MinLength(3, ErrorMessage = "A categoria deve ter pelo menos 3 caracteres")]
+        [MaxLength(120, ErrorMessage = "A categoria deve ter pelo menos 3 caracteres")]
         public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } 
+        [Required(ErrorMessage = "Informe o preço do produto")]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
         public int CategoriaId { get; set; }
